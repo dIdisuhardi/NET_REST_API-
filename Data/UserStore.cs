071f236e5fd3ca1b97389d1bacdd63c642340828
DIdisuhardi <didisuhardi00@gmail.com>
2023-11-21 08:56:23 +0700

    Add project files.

---
+++ Data/UserStore.cs
@@ -0,0 +1,13 @@
+﻿using Kel33_Mod9_TugasAPI.Models.Dto;
+
+namespace Kel33_Mod9_TugasAPI.Data
+{
+    public static class UserStore
+    {
+        public static List<UserDTO> userList = new List<UserDTO>
+        {
+             new UserDTO{Id=1, Username="Didi", Password="Suhardi"},
+             new UserDTO{Id=2, Username="Uruha", Password="Rushia"}
+        };
+    }
+}
